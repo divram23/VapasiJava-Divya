3f6d9d96e0976d7f3b765fb5fcec58e77aa797d0
Divya <divya.2387@gmail.com>
2021-03-01 20:25:32 +0530

    Level1 -DataTypes assignments

---
+++ src/AssignmentJavaFundamentals/DataTypes/PrintDatatypes.java
@@ -0,0 +1,15 @@
+package AssignmentJavaFundamentals.DataTypes;
+
+public class PrintDatatypes {
+    public static void main(String[] args) {
+        int num = 10;
+        System.out.println("Integer:" +num);
+
+        double d_num = 87767.5555875;
+        System.out.println("Double: "+d_num);
+
+        char name = 'D';
+        System.out.println("Character :"+name);
+
+    }
+}
