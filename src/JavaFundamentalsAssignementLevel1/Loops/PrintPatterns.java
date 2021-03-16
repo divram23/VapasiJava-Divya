9c00b6e2a2e9738e3aa7c4b42a5f9165a9617b7f
Divya <divya.2387@gmail.com>
2021-03-16 19:11:05 +0530

    Updated file on Mar 16th - Removed commented code

--- src/JavaFundamentalsAssignementLevel1/Loops/PrintPatterns.java
+++ src/JavaFundamentalsAssignementLevel1/Loops/PrintPatterns.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class PrintPatterns {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter PatternDesign(1/2/3) number:");
+        System.out.println("Enter PatternDesign(1/2) number:");
         int n = scanner.nextInt();
 
         if (n == 1) {
@@ -43,44 +43,6 @@ public class PrintPatterns {
                 System.out.println("");
             }
         }
-        /*else if(n==3){
-            int rows=4;
-            int m=1;
-            for (int i= 0; i<= rows-1; i++)
-            {
-                for (int j=0; j<=i; j++)
-                {
-                    System.out.print(" ");
-                }
-
-
-                for (int k=0; k<=rows-1-i; k++) {
-                    if (k % 2 == 0) {//m =0;
-                        System.out.print("1");
-                    } else {
-                        int num = 1;
-                        System.out.print("0" + "1");
-                        num = (num == 0)? 1 : 0;
-                        System.out.print(num);
-
-                    }
-                }
-                System.out.println();
-            }*/
-           /* int rows = 4;
-            for (int i= 0; i<= rows-1; i++)
-            {
-                for (int j=0; j<=i; j++)
-                {
-                    System.out.print(" ");
-                }
-                for (int k=0; k<=rows-1-i; k++)
-                {
-                    System.out.print("*" + " ");
-                }
-                System.out.println();
-            }*/
-
 
 
         else
